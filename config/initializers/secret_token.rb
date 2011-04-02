df55404d4b0f6a633c58bec9afc28e56d07b4304
officialjunk <officialjunk@gmail.com>
2011-04-02 05:50:04 -0700

    t=0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Staticapp::Application.config.secret_token = '8c1741af579945ea646e85fef34f8c67eb084d2015088ecb6ff3a238b8338f90e37cb15114d77cd51c0fb228689da686fe7eda48722de9970782b26074d907ab'
